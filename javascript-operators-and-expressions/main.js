d5780c58f0f31c397abd03bfd52cfbf22079f8d9
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-05-16 20:01:18 +0000

    Completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,69 @@
+var width;
+var height;
+var area;
+
+width = 5;
+height = 6;
+area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill;
+var payment;
+var change;
+
+bill = 35;
+payment = 40;
+change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes;
+var midterm;
+var final;
+var grade;
+
+quizzes = 95;
+midterm = 75;
+final = 90;
+grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName;
+var lastName;
+var fullName;
+
+firstName = 'Bryan';
+lastName = 'Valdovinos';
+fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH;
+var isAcidic;
+
+pH = 9;
+isAcidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount;
+var isSparta;
+
+headCount = 151;
+isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto;
+
+motto = fullName + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
