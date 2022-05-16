e1e60017a961aeee8b30195c0743408fd376fc09
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-05-16 17:30:04 +0000

    Completed javascript-primitives-and-variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+var fullName = 'Bryan Valdovinos';
+console.log('value of fullName:', fullName);
+
+var isCool = true;
+console.log('value of isCool', isCool);
+
+var totalPets = 0;
+console.log('value of totalPets:', totalPets);
+
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never =
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
