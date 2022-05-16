ebcfbd9c42c220f5ef88d2de1dba0eb6c6b8976d
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-05-16 22:37:01 +0000

    value & declaration on same line now

--- javascript-operators-and-expressions/main.js
+++ javascript-operators-and-expressions/main.js
@@ -1,70 +1,45 @@
-var width;
-var height;
-var area;
-
-width = 5;
-height = 6;
-area = width * height;
+var width = 5;
+var height = 6;
+var area = width * height;
 
 console.log('value of area:', area);
 console.log('typeof area:', typeof area);
 
-var bill;
-var payment;
-var change;
-
-bill = 35;
-payment = 40;
-change = payment - bill;
+var bill = 35;
+var payment = 40;
+var change = payment - bill;
 
 console.log('value of change:', change);
 console.log('typeof change:', typeof change);
 
-var quizzes;
-var midterm;
-var final;
-var grade;
-
-quizzes = 95;
-midterm = 75;
-final = 90;
-grade = (quizzes + midterm + final) / 3;
+var quizzes = 95;
+var midterm = 75;
+var final = 90;
+var grade = (quizzes + midterm + final) / 3;
 
 console.log('value of grade:', grade);
 console.log('typeof grade:', typeof grade);
 
-var firstName;
-var lastName;
-var fullName;
-
-firstName = 'Bryan';
-lastName = 'Valdovinos';
-fullName = firstName + ' ' + lastName;
+var firstName = 'Bryan';
+var lastName = 'Valdovinos';
+var fullName = firstName + ' ' + lastName;
 
 console.log('value of fullName:', fullName);
 console.log('typeof fullName:', typeof fullName);
 
-var pH;
-var isAcidic;
-
-pH = 9;
-isAcidic = pH < 7;
+var pH = 9;
+var isAcidic = pH < 7;
 
 console.log('value of isAcidic:', isAcidic);
 console.log('typeof isAcidic:', typeof isAcidic);
 
-var headCount;
-var isSparta;
-
-headCount = 151;
-isSparta = headCount === 300;
+var headCount = 151;
+var isSparta = headCount === 300;
 
 console.log('value of isSparta:', isSparta);
 console.log('typeof isSparta:', typeof isSparta);
 
-var motto;
-
-motto = fullName;
+var motto = fullName;
 
 motto += ' is the GOAT';
 console.log('value of motto:', motto);
