d244d3c3a5a9b87e3d870db0dcb616d853e5c7ee
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-05-17 23:38:07 +0000

    Moved items under same object

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -8,16 +8,16 @@ var orderHistory = [
       orderPlaced: 'August 4, 2020',
       deliveryStatus: ['Yes', 'No'],
       deliveryDate: ['August 8, 2020', 'In Transit'],
-      returnWindow: 'Closed on Sep 7, 2020'
-    },
-
-    productDetails: {
-      productName: 'Javascript for impatient programmers',
-      price: 31.55,
-      author: ['Yes', 'No'],
-      authorName: ['Dr. Axel Rauschmayer', 'No Author']
+      returnWindow: 'Closed on Sep 7, 2020',
+      items: [
+        {
+          productName: 'Javascript for impatient programmers',
+          price: 31.55,
+          author: ['Yes', 'No'],
+          authorName: ['Dr. Axel Rauschmayer', 'No Author']
+        }
+      ]
     }
-
   },
 
   {
@@ -28,16 +28,16 @@ var orderHistory = [
       orderPlaced: 'July 19, 2020',
       deliveryStatus: ['Yes', 'No'],
       deliveryDate: ['Jul 20, 2020', 'In Transit'],
-      returnWindow: 'Closed on Aug 19, 2020'
-    },
-
-    productDetails: {
-      productName: 'The Timeless Way of Building',
-      price: 41.33,
-      author: ['Yes', 'No'],
-      authorName: ['Christopher Alexander', 'No Author']
+      returnWindow: 'Closed on Aug 19, 2020',
+      items: [
+        {
+          productName: 'The Timeless Way of Building',
+          price: 41.33,
+          author: ['Yes', 'No'],
+          authorName: ['Christopher Alexander', 'No Author']
+        }
+      ]
     }
-
   },
 
   {
@@ -48,16 +48,16 @@ var orderHistory = [
       orderPlaced: 'July  4, 2020',
       deliveryStatus: ['Yes', 'No'],
       deliveryDate: ['July 7, 2020', 'In Transit'],
-      returnWindow: 'Closed on August 5, 2020'
-    },
-
-    productDetails: {
-      productName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-      price: 15.98,
-      author: ['Yes', 'No'],
-      authorName: [null, 'No Author']
+      returnWindow: 'Closed on August 5, 2020',
+      items: [
+        {
+          productName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+          price: 15.98,
+          author: ['Yes', 'No'],
+          authorName: [null, 'No Author']
+        }
+      ]
     }
-
   },
 
   {
@@ -68,36 +68,21 @@ var orderHistory = [
       orderPlaced: 'July 3, 2020',
       deliveryStatus: ['Yes', 'No'],
       deliveryDate: ['July 5, 2020', 'In Transit'],
-      returnWindow: 'Closed on August 4, 2020'
-    },
-
-    productDetails: {
-      productName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-      price: 94.95,
-      author: ['Yes', 'No'],
-      authorName: [null, 'No Author']
+      returnWindow: 'Closed on August 4, 2020',
+      items: [
+        {
+          productName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+          price: 94.95,
+          author: ['Yes', 'No'],
+          authorName: [null, 'No Author']
+        },
+        {
+          productName: 'The Art of Sql',
+          price: 33.99,
+          author: ['Yes', 'No'],
+          authorName: ['Stephanie, Faroult', 'No Author']
+        }
+      ]
     }
-
-  },
-
-  {
-    orderDetails: {
-      orderNumber: '113-2883177-2648248',
-      shipTo: 'JS Masher',
-      total: 138.93,
-      orderPlaced: 'July 3, 2020',
-      deliveryStatus: ['Yes', 'No'],
-      deliveryDate: ['July 5, 2020', 'In Transit'],
-      returnWindow: 'Closed on August 4, 2020'
-    },
-
-    productDetails: {
-      productName: 'The Art of Sql',
-      price: 33.99,
-      author: ['Yes', 'No'],
-      authorName: ['Stephanie, Faroult', 'No Author']
-    }
-
   }
-
 ];
