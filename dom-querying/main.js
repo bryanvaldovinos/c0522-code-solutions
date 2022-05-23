e5cf1b166939d622623544ec67e0d9db51e4c7e8
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-05-23 18:02:31 +0000

    Added console.dir

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,11 +1,16 @@
 console.log('hello, world');
 
 console.log(document.querySelector('h1'));
+console.dir(document.querySelector('h1'));
 
 console.log(document.querySelector('#explanation'));
+console.dir(document.querySelector('#explanation'));
 
 console.log(document.querySelector('.hint'));
+console.dir(document.querySelector('.hint'));
 
 console.log(document.querySelectorAll('p'));
+console.dir(document.querySelector('p'));
 
 console.log(document.querySelectorAll('.example-link'));
+console.dir(document.querySelector('.example-link'));
