240a09c74bcbc813ee66cdd00578bb179295a577
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-05-23 19:11:41 +0000

    Still working

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -2,18 +2,10 @@
 function isVowel(char) {
   if (char.length === 1) {
     var vowels = 'aeiouAEIOU';
-    for (var i = 0; i < vowels.length; i++) {
-      if (vowels[i] >= 0) {
-        return i;
-      }
-      var isVowel = vowels[i];
-    }
-    // var isVowel = vowels.indexOf(char) >= 0;
-
+    var isVowel = vowels.indexOf(char) >= 0;
     return isVowel;
   }
 }
-
 // return boolean true or false to check if char is vowel
 // need to account for cap and lowercase
 // its a single char so check for that with if?
