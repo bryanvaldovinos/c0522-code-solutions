b68ab87c6a21454e4acdb9580b960df8cc867fef
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-05-27 07:58:57 +0000

    Completed javascript-typing-tutor challenge

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,7 +1,15 @@
-var magicWord = document.querySelectorAll('span');
+var span = document.querySelectorAll('span');
+var count = 0;
+span[count].setAttribute('class', 'underline');
 
-function typer(event) {
-
+function tutor(event) {
+  if (span[count].textContent === event.key) {
+    count++;
+    span[count - 1].className = 'green';
+    span[count].className = 'underline';
+  } else if (span[count].textContent !== event.key) {
+    span[count].className = 'red underline';
+  }
 }
 
-magicWord.addEventListener('keydown', typer);
+document.addEventListener('keydown', tutor);
