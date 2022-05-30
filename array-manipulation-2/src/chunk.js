ca9366e376de08859d8fcdc1597f4b5e720c3c0d
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-05-30 08:38:23 +0000

    Completed array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,8 +1,24 @@
 /* exported chunk */
 function chunk(array, size) {
   var newArray = [];
-  for (var i = 0; newArray.length < (array.length / size); i++) {
-    newArray.push([]);
+  for (var i = 0; i < array.length; i++) {
+    var b = newArray[newArray.length - 1];
+    if (!b || b.length === size) {
+      newArray.push([array[i]]);
+    } else {
+      b.push(array[i]);
+    }
   }
   return newArray;
 }
+
+// need to return new array of sub arrays equal to size
+// leftover elements are in own sub array
+// will have to loop through each element of array to work with
+// if length of new array copy is equal to size then push a new sub
+// array to new array
+// else push element at index of i of array into new array copy
+// loop will continue running through all elements
+// and will sort out to either new sub array or same sub array
+// depending on length;
+// finally return new array
