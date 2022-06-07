394db5dcae853e1fd1591db151e6243d08fbd767
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-06-07 04:49:51 +0000

    still working object 2

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,10 @@
 /* exported omit */
+function omit(source, keys) {
+  var obj = {};
+  for (var i = 0; i < keys.length; i++) {
+    for (var property in source) {
+      obj[keys[i]] = source[property];
+    }
+  }
+  return obj;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -3,9 +3,9 @@ function pick(source, keys) {
   var obj = {};
   for (var i = 0; i < keys.length; i++) {
     obj[keys[i]] = source[keys[i]];
-  }
-  if (obj[keys[i]] !== [keys[i]]) {
-    obj = {};
+    if (obj[keys[i]] === undefined) {
+      delete obj[keys[i]];
+    }
   }
   return obj;
 }
