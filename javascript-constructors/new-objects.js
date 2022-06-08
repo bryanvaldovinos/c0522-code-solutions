9eaaefa2b623a3c3fb01b07ddc045b09cf463a50
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-06-08 19:00:49 +0000

    edited new objects

--- javascript-constructors/new-objects.js
+++ javascript-constructors/new-objects.js
@@ -2,22 +2,22 @@ var anObject = new Object();
 console.log('value of anObject:', anObject);
 console.log('typeof anObject:', typeof anObject);
 
-var anArray = new Object([]);
+var anArray = new Array();
 console.log('value of anArray:', anArray);
 console.log('typeof anArray:', typeof anArray);
 
-var aFunction = new Object(function () {});
+var aFunction = new Function();
 console.log('value of aFunction:', aFunction);
 console.log('typeof aFunction:', typeof aFunction);
 
-var aString = new Object('string');
+var aString = new String();
 console.log('value of aString:', aString);
 console.log('typeof aString:', typeof aString);
 
-var aNumber = new Object(24);
+var aNumber = new Number();
 console.log('value of aNumber:', aNumber);
 console.log('typeof aNumber:', typeof aNumber);
 
-var aBoolean = new Object(true);
+var aBoolean = new Boolean();
 console.log('value of aBoolean:', aBoolean);
 console.log('typeof aBoolean:', typeof aBoolean);
