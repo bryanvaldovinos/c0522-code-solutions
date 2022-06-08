e6a445319f7ebba8a0a392e4a6e8b75c4ad5b2a2
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-06-08 02:12:42 +0000

    transaction & account done, bank to do

---
+++ javascript-bank/account.js
@@ -0,0 +1,42 @@
+/* exported Account */
+function Account(number, holder, transactions) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+
+var accInfo = new Account();
+
+Account.prototype.deposit = function (amount) {
+  var deposit = new Transaction('deposit', amount);
+  if (!(Number.isInteger(amount)) || (amount < 1)) {
+    return false;
+  } else {
+    this.transactions.push(deposit);
+    return true;
+  }
+};
+
+Account.prototype.withdraw = function (amount) {
+  var withdrawal = new Transaction('withdrawal', amount);
+  if (!(Number.isInteger(amount)) || (amount < 1)) {
+    return false;
+  } else {
+    this.transactions.push(withdrawal);
+    return true;
+  }
+};
+
+Account.prototype.getBalance = function () {
+  var Dep = 0;
+  var With = 0;
+  for (var i = 0; i < this.transactions.length; i++) {
+    if (this.transactions[i].type === 'deposit') {
+      Dep += this.transactions[i].amount;
+    } else if (this.transactions[i].type !== 'withdraw') {
+      With += this.transactions[i].amount;
+    }
+  }
+  var balance = Dep - With;
+  return balance;
+};
