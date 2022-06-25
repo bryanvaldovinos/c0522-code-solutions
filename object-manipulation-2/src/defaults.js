99d22ed4344d89d6bc20086aa6a88f53da427575
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-06-25 08:01:09 +0000

    Completed object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,10 +1,8 @@
 /* exported defaults */
 function defaults(target, source) {
-  for (var property in target) {
-    for (var key in source) {
-      if (target[property] !== source[key]) {
-        target[property] = source[key];
-      }
+  for (var key in source) {
+    if (target[key] === undefined) {
+      target[key] = source[key];
     }
   }
 }
