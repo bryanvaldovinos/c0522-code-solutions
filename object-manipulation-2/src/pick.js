4583131b0fc293236cfa2adcd980d2cc4710b48d
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-06-27 22:17:47 +0000

    object-manipulation-2 redone

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -2,9 +2,8 @@
 function pick(source, keys) {
   var obj = {};
   for (var i = 0; i < keys.length; i++) {
-    obj[keys[i]] = source[keys[i]];
-    if (obj[keys[i]] === undefined) {
-      delete obj[keys[i]];
+    if (source[keys[i]] !== undefined) {
+      obj[keys[i]] = source[keys[i]];
     }
   }
   return obj;
