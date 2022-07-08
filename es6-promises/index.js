499bcd4ef42b1515805b1bd3d97aaeed97d642e2
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-07-08 18:52:12 +0000

    condensed code

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -2,11 +2,6 @@ const takeAChance = require('./take-a-chance');
 
 const chance = takeAChance('Bryan');
 
-chance.then(resolve => {
-  console.log(resolve);
-});
-
-chance.catch(reject => {
-  const e = reject.message;
-  console.error(e);
-});
+chance
+  .then(resolve => { console.log(resolve); })
+  .catch(reject => { const e = reject.message; console.error(e); });
