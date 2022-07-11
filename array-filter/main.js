cc7192a8003ffddf041c163c5c87bc7e0a3b7a5b
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-07-11 17:28:19 +0000

    array-filter exercise complete

---
+++ array-filter/main.js
@@ -0,0 +1,31 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna'
+];
+
+const even = numbers.filter(number => {
+  return number % 2 === 0;
+});
+
+const overFive = numbers.filter(number => {
+  return number > 5;
+});
+
+const eStart = names.filter(name => {
+  return name.startsWith('E');
+});
+
+const nameD = names.filter(name => {
+  return name.toLowerCase().includes('d');
+});
+
+console.log(even, overFive, eStart, nameD);
