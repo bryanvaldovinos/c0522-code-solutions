cd8b8131cf60dc9e08a4b1d80cd59cb4c5d3e218
Bryan Valdovinos <bvaldovinos24@gmail.com>
2022-07-15 17:54:09 +0000

    react-form-controls exercise complete

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class RegistrationForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUsernameChange(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    console.log(this.state);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Username
+        <input value={this.state.username} onChange={this.handleUsernameChange}></input>
+        </label>
+        <label>
+          Password
+          <input value={this.state.password} type="password" onChange={this.handlePasswordChange}></input>
+        </label>
+        <button onClick={this.handleSubmit} type="button">Sign Up</button>
+      </form>
+    );
+  }
+}
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<RegistrationForm />);
